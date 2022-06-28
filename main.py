a0c6371c97b08d801d4f8c37c4b3e9c0b18f617d
Michael Ansbro <mransbro@gmail.com>
2022-06-28 21:55:33 +0100

    adding files

--- main.py
+++ main.py
@@ -1,55 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-Downloads all tweets from a given user.
-Uses twitter.Api.GetUserTimeline to retreive the last 3,200 tweets from a user.
-Twitter doesn't allow retreiving more tweets than this through the API, so we get
-as many as possible.
-t.py should contain the imported variables.
-"""
-
-
-
-from __future__ import print_function
-
+import requests
+import os
 import json
-import sys
 
-import twitter
-from env import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
+# To set your environment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = os.environ.get("BEARER_TOKEN")
+
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+query_params = {"query": "#bbcnews lang:en"}
 
 
-def get_tweets(api=None, screen_name=None):
-    timeline = api.GetUserTimeline(screen_name=screen_name, count=200)
-    earliest_tweet = min(timeline, key=lambda x: x.id).id
-    print("getting tweets before:", earliest_tweet)
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
 
-    while True:
-        tweets = api.GetUserTimeline(
-            screen_name=screen_name, max_id=earliest_tweet, count=200
-        )
-        new_earliest = min(tweets, key=lambda x: x.id).id
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
 
-        if not tweets or new_earliest == earliest_tweet:
-            break
-        else:
-            earliest_tweet = new_earliest
-            print("getting tweets before:", earliest_tweet)
-            timeline += tweets
 
-    return timeline
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
 if __name__ == "__main__":
-    api = twitter.Api(
-        CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
-    )
-    screen_name = sys.argv[1]
-    print(screen_name)
-    timeline = get_tweets(api=api, screen_name=screen_name)
-
-    with open('examples/timeline.json', 'w+') as f:
-        for tweet in timeline:
-            f.write(json.dumps(tweet._json))
-            f.write('\n')
+    main()
